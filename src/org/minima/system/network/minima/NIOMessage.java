5ba3a2a8c5b9beefadda179bcc58be5ce4422104
serg-83 <serg-83@users.noreply.github.com>
2026-02-21 08:15:13 +0300

    Fix build: add missing HashSet import in NIOMessage Исправление сборки: добавлен недостающий импорт HashSet в NIOMessage

--- src/org/minima/system/network/minima/NIOMessage.java
+++ src/org/minima/system/network/minima/NIOMessage.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
